dd065437f01703c3f3be75a8c96571d80d5adf51
spokenpleinair <annetsuei@gmail.com>
2021-04-08 19:57:27 -0700

    Update

---
+++ Homework/JS_03/js/index.js
@@ -0,0 +1,13 @@
+// Create the madlibs function here.
+function stringSum() {
+var myVariable = 10;
+var variableTwo = 5;
+
+myVariable = myVariable + 20; 
+myVariable = myVariable / 3; 
+myVariable = myVariable * variableTwo; 
+
+    console.log(myVariable);
+}
+
+stringSum();
