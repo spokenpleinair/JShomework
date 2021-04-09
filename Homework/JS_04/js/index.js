0035b02308d3e15fc1e7f4e984d7f1ef6cdaf793
spokenpleinair <annetsuei@gmail.com>
2021-04-08 20:08:18 -0700

    updates

---
+++ Homework/JS_04/js/index.js
@@ -0,0 +1,10 @@
+// Create the sum of two numbers function here.
+function stringSumTwoNumbers () {
+    var variableOne = 1;
+    var variableTwo = 2;
+    var finalSum = variableOne + variableTwo;
+    console.log (finalSum);
+    }
+    
+
+stringSumTwoNumbers();
---
+++ Homework/JS_05/js/index.js
@@ -0,0 +1,10 @@
+// Create the minute to seconds conversion function here.
+function convertMinuteSeconds () {
+    var minutes = 1;
+    var seconds = 60;
+    var totalSeconds = minutes * seconds;
+    console.log (totalSeconds);
+    }
+    
+    convertMinuteSeconds();
+    
