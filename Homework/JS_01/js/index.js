66f80b03e31cba86afe9c9d5528d2d4470fa7965
spokenpleinair <annetsuei@gmail.com>
2021-04-08 19:52:34 -0700

    Drop files

---
+++ Homework/JS_01/js/index.js
@@ -0,0 +1,10 @@
+// Create the madlibs function here.
+function stringDocRx() {
+    var ingredient = 'lime';
+    var container = 'coconut';
+
+    var shake = "Put" + " " + "the" + " " + ingredient + " " + "in" + " " + "the" + " " + container + " " + "then" + " " + "you" + " " + "shake" + " " + "them" + " " + "both" + " " +"up.";
+    console.log(shake);
+}
+
+stringDocRx();
